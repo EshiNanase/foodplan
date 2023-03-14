6754c914ee04a9862c9b07151985691fbb50f776
Nanashi <alksndr.zln@gmail.com>
2023-03-14 20:16:47 +0300

    Initial commit, created two apps: authorization, recipes, moved environment variables to .env

---
+++ authorization/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'auth.html'
+
+
+class RegistrationView(TemplateView):
+    template_name = 'registration.html'
